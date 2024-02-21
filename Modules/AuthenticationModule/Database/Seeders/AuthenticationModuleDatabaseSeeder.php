8a596839b5c9564e2299ead9eeb71f9674b2c4b4
chhaily <chhailyly1681682gmail.com>
2024-02-21 19:53:27 +0700

    first commit

---
+++ Modules/AuthenticationModule/Database/Seeders/AuthenticationModuleDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\AuthenticationModule\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AuthenticationModuleDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
